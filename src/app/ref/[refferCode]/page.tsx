a98a206340c99c0230f02666debb6f06cc2784cc
ReAuCoin <lucasbolla.uk@gmail.com>
2025-04-27 21:32:49 +0100

    0.09.2

--- src/app/ref/[refferCode]/page.tsx
+++ src/app/ref/[refferCode]/page.tsx
@@ -1,9 +1,11 @@
 // src/app/ref/[refferCode]/page.tsx
 
-import AirDropAmount from "@/components/AirDropAmount";
-import RefSocial from "@/components/RefSocial";
+'use client';
 
-export const dynamic = "force-dynamic";
+import AirDropAmount from '@/components/AirDropAmount';
+import RefSocial from '@/components/RefSocial';
+
+export const dynamic = 'force-dynamic';
 
 export default async function RefPage({
   params,
@@ -14,8 +16,15 @@ export default async function RefPage({
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-4">
+      {/* Total de Airdrop */}
+      <div className="w-full max-w-md">
+        <AirDropAmount />
+      </div>
+
       {/* Seção de compartilhamento/referral */}
-      <RefSocial refferCode={refferCode} />
+      <div className="mt-8 w-full max-w-md">
+        <RefSocial refferCode={refferCode} />
+      </div>
     </main>
   );
 }
